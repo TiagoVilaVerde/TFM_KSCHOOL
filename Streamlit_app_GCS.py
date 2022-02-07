ba557f7d9eff77f7c21560eb90d2b569171bf98e
TiagoVilaVerde <82608671+TiagoVilaVerde@users.noreply.github.com>
2022-02-07 21:39:18 +0100

    Update Streamlit_app_GCS.py

--- Streamlit_app_GCS.py
+++ Streamlit_app_GCS.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(layout="wide")
+#st.set_page_config(layout="wide")
 
 st.title('Top European Football National Leagues Predictions')
 
