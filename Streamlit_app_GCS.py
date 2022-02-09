f90df36de56d3a176e20e7dfd0f53b544649c5a0
TiagoVilaVerde <82608671+TiagoVilaVerde@users.noreply.github.com>
2022-02-09 22:04:37 +0100

    Update Streamlit_app_GCS.py

--- Streamlit_app_GCS.py
+++ Streamlit_app_GCS.py
@@ -1,6 +1,21 @@
 import streamlit as st
 import pandas as pd
 
+CURRENT_THEME = "blue"
+IS_DARK_THEME = True
+EXPANDER_TEXT = """
+    This is a custom theme. You can enable it by copying the following code
+    to `.streamlit/config.toml`:
+    ```python
+    [theme]
+    primaryColor = "#E694FF"
+    backgroundColor = "#00172B"
+    secondaryBackgroundColor = "#0083B8"
+    textColor = "#C6CDD4"
+    font = "sans-serif"
+    ```
+    """
+
 st.set_page_config(layout="wide")
 
 st.title('Top European Football National Leagues Predictions')
